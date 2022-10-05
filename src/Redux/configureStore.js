fe183b11b47833d60587d2d434ab9ddb26aa34b4
lucabenaventew <cacachuuuluca@gmail.com>
2022-10-05 12:14:07 -0500

    Clean code

--- src/Redux/configureStore.js
+++ src/Redux/configureStore.js
@@ -1,12 +1,10 @@
 import { configureStore } from '@reduxjs/toolkit';
-// import logger from 'redux-logger';
 import herosSlice from './HomeReducer/HomeReducer';
 
 const store = configureStore({
   reducer: {
     heros: herosSlice,
   },
-  // middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(logger),
 });
 
 export default store;
